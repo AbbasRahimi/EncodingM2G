685284033d49613da01888de3e8b484771c68d5b
AbbasRahimi <Abbasrahimi71@gmail.com>
2021-07-10 17:56:31 +0430

    adding relation type

--- src/test.py
+++ src/test.py
@@ -2,9 +2,9 @@ import src.encodeM2G as encoder
 
 
 if __name__ == "__main__":
-    # ENCODE_M2G(metamodel_name="x2.ecore", model_name="result999_1.xmi")
-    # ENCODE_M2G(metamodel_name="x1.ecore", model_name="result0_1.xmi")
-    encoder.ENCODE_M2G(metamodel_name="AIDS.ecore", model_name="graph2.xmi")
-    # ENCODE_M2G(metamodel_name="Tree.ecore", model_name="tree.xmi")
-    # ENCODE_M2G(metamodel_name="fsa.ecore", model_name="FSAModel.xmi")
-    # ENCODE_M2G(metamodel_name="basicfamily.ecore", model_name="FamilyRoot.xmi")
+    # encoder.ENCODE_M2G(metamodel_name="x2.ecore", model_name="result999_1.xmi")
+    encoder.ENCODE_M2G(metamodel_name="x1.ecore", model_name="result0_1.xmi")
+    # encoder.ENCODE_M2G(metamodel_name="AIDS.ecore", model_name="graph2.xmi")
+    # encoder.ENCODE_M2G(metamodel_name="Tree.ecore", model_name="tree.xmi")
+    # encoder.ENCODE_M2G(metamodel_name="fsa.ecore", model_name="FSAModel.xmi")
+    # encoder.ENCODE_M2G(metamodel_name="basicfamily.ecore", model_name="FamilyRoot.xmi")
